3b629bfff0b52e70bfd9f5a98d643da6f9c89ea8
George Liu <georgeliu8110@gmail.com>
2024-06-11 17:14:15 -0500

    refactor appointments confirmation page

--- client/app/appointmentconfirmed/page.jsx
+++ client/app/appointmentconfirmed/page.jsx
@@ -8,29 +8,8 @@ import { Suspense } from "react";
 import { app } from "../firebase/config";
 
 export default function AppointmentConfirmationPage() {
-  // const searchParams = useSearchParams();
-  // const appointment = searchParams.get("appointment");
-
-  // let appointmentData = { day: "no day", timeSlot: "no timeslot" };
-  // let confirmId = "no id";
-  // let customer = { name: "no name" };
-
-  // if (appointment != null && appointment != "undefined") {
-  //   try {
-  //     const { appointmentInfo, confirmationId, customerInfo } =
-  //       JSON.parse(appointment);
-  //     appointmentData = appointmentInfo;
-  //     confirmId = confirmationId;
-  //     customer = customerInfo;
-  //   } catch (error) {
-  //     appointmentData = { day: "no day", timeSlot: "no timeslot" };
-  //     confirmId = "error saving appointment";
-  //     customer = { name: "no name" };
-  //   }
-  // }
-
   return (
-    <Suspense>
+    <Suspense fallback={<p>Loading ... </p>}>
       <AppointmentInfo />
     </Suspense>
   );
@@ -39,6 +18,7 @@ export default function AppointmentConfirmationPage() {
 function AppointmentInfo() {
   const searchParams = useSearchParams();
   const appointment = searchParams.get("appointment");
+  console.log("appointment", appointment);
 
   let appointmentData = { day: "no day", timeSlot: "no timeslot" };
   let confirmId = "no id";
@@ -46,7 +26,7 @@ function AppointmentInfo() {
 
   if (appointment != null && appointment != "undefined") {
     try {
-      const { day, timeSlot, confirmationID } = JSON.parse(appointment);
+      const { date: day, timeSlot, confirmationNumber: confirmationID } = JSON.parse(appointment);
       appointmentData = {
         day,
         timeSlot,
@@ -62,11 +42,11 @@ function AppointmentInfo() {
   return (
     <div className="card flex items-center bg-white p-8 rounded-none">
       <div className="card-body items-center border-black border-2 rounded-lg text-black">
-        <h1 className="card-title">Your booking is confirmed</h1>
-        <p>See you soon</p>
+        <h1 className="card-title">Your booking is confirmed ! </h1>
+        <p>See you soon !</p>
         {/* <p>Service Name</p> */}
         <p>Confirmation Id: {confirmId}</p>
-        <p>Date:{formatDate(appointmentData.day)}</p>
+        <p>Date: {formatDate(appointmentData.day)}</p>
         <p>Time: {timeSlots[appointmentData.timeSlot]}</p>
         <div className="card-actions justify-end">
           <Link href={"/appointment"}>
