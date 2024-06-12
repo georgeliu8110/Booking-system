822c6327991c7aa96e2564203fd67fb4c27e82cc
George Liu <georgeliu8110@gmail.com>
2024-06-11 22:42:41 -0500

    update firebase/firestore config

--- client/app/firestore/config.js
+++ client/app/firestore/config.js
@@ -1,7 +1,7 @@
 const { initializeApp, applicationDefault, cert, getApps } = require('firebase-admin/app');
 const { getFirestore, Timestamp, FieldValue, Filter } = require('firebase-admin/firestore');
 
-const serviceAccount = require('/Users/zheliu/Desktop/Booking-system/client/app/firestore/dsd-cohort-2024-c9d62ba8cd0b.json');
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
 
 if (!getApps().length) {
   initializeApp({
