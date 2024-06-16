052a83165a25d09ebd55a263cc08c4416732cd80
George Liu <georgeliu8110@gmail.com>
2024-06-16 14:58:54 -0500

    seperate code for navBar

---
+++ client/app/components/NavBar.jsx
@@ -0,0 +1,87 @@
+"use client";
+
+import Link from "next/link";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "@/app/firebase/config";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useEffect, useState } from "react";
+import { usePathname } from "next/navigation";
+import useDrift from "@/app/_hooks/liveChat-api/useDrift.js";
+import Image from 'next/image';
+import ThemeChanger from '@/app/components/ThemeChanger';
+
+export default function NavBar({ children }) {
+  const [user, loading, error] = useAuthState(auth);
+
+  useDrift();
+  const [shouldUseDrift, setShouldUseDrift] = useState(true);
+  const pathname = usePathname();
+
+  useEffect(() => {
+    if (typeof window === "undefined") return;
+
+    let adminURL = pathname.includes("admin");
+
+    if (adminURL && window.drift?.api?.widget) {
+      window.drift.api.widget.hide();
+    } else if (!adminURL && window.drift?.api?.widget) {
+      window.drift.api.widget.show();
+    }
+    setShouldUseDrift(adminURL ? false : true);
+  }, [pathname]);
+
+  return (
+        <div className="w-auto mx-3 mt-3 px-4 py-8">
+          <header className="flex border rounded-xl p-1 items-center bg-gray-100 dark:bg-black">
+          <div className="navbar marker:bg-gray-100 rounded-box">
+            <div className="flex-1 px-2 lg:flex-none items-center justify-center">
+              <Link href={"/"} className="text-3xl font-bold flex items-center">
+                <Image priority src={'/broken-pipe.svg'} alt='plumbing pipe repair' width={120} height={120}/>
+                Broken Pipe Plumbing
+              </Link>
+            </div>
+            <div className="flex justify-end flex-1 px-2">
+              <div className="flex items-stretch">
+                <Link href={"/appointment"} className="btn btn-ghost rounded-btn text-xl">Book Now</Link>
+                <Link href={"/companyservices"} className="btn btn-ghost rounded-btn text-xl">Services</Link>
+                <div className="dropdown dropdown-end">
+                  <div tabIndex={0} role="button" className="btn btn-ghost rounded-btn text-xl">Client Portal</div>
+                  <ul tabIndex={0} className="menu dropdown-content z-[1] p-2 shadow bg-base-100 rounded-box w-52 mt-4">
+                  <li>
+                      <Link href={"/signup"}>
+                      <div className="font-bold rounded-lg">
+                        Sign Up
+                      </div>
+                    </Link>
+                    </li>
+                    <li>
+                      <Link href={"/login"}>
+                      <div className="font-bold rounded-lg">
+                        Login
+                      </div>
+                    </Link>
+                    </li>
+                  </ul>
+                </div>
+                <div className="dropdown dropdown-end">
+                  <div tabIndex={0} role="button" className="btn btn-ghost rounded-btn text-xl">Employee Portal</div>
+                  <ul tabIndex={0} className="menu dropdown-content z-[1] p-2 shadow bg-base-100 rounded-box w-52 mt-4">
+                    <li>
+                      <Link href={'/adminSignInAndSignUp'}>
+                      <div className="font-bold rounded-lg ">
+                        Admin Management
+                      </div>
+                    </Link>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <ThemeChanger />
+            </div>
+          </div>
+          </header>
+          {children}
+        </div>
+  );
+}
+
--- client/app/layout.jsx
+++ client/app/layout.jsx
@@ -1,82 +1,127 @@
-"use client";
-
 import "./globals.css";
-import Link from "next/link";
-import { onAuthStateChanged } from "firebase/auth";
-import { auth } from "./firebase/config";
-import { useAuthState } from "react-firebase-hooks/auth";
-import { useEffect, useState } from "react";
-import { usePathname } from "next/navigation";
-import useDrift from "./_hooks/liveChat-api/useDrift.js";
+import { ThemeProvider } from 'next-themes'
+import Navbar from '@/app/components/NavBar';
 
 export default function RootLayout({ children }) {
-  const [user, loading, error] = useAuthState(auth);
-
-  useDrift();
-  const [shouldUseDrift, setShouldUseDrift] = useState(true);
-  const pathname = usePathname();
-
-  useEffect(() => {
-    if (typeof window === "undefined") return;
-
-    let adminURL = pathname.includes("admin");
-
-    console.log(pathname, shouldUseDrift);
-    if (adminURL && window.drift?.api?.widget) {
-      window.drift.api.widget.hide();
-    } else if (!adminURL && window.drift?.api?.widget) {
-      window.drift.api.widget.show();
-    }
-    setShouldUseDrift(adminURL ? false : true);
-  }, [pathname]);
 
   return (
+
     <html
       lang="en"
-      data-theme="light">
-      <body>
-        <div className="max-w-screen-2xl mx-auto px-4 py-8">
-          <header className="flex bg-gray-100 border rounded-xl p-6 items-center">
-            <Link
-              href={"/"}
-              className="font-bold text-xl flex-1">
-              <div>Broken Pipe Plumbing</div>
-            </Link>
-            <nav className="flex gap-4 items-center">
-              <Link href={"/appointment"}>
-                <div className="font-bold">Book Now</div>
-              </Link>
-
-              <Link href={"/companyservices"}>
-                <div className="font-bold">Services</div>
-              </Link>
-              {!user && (
-                <>
-                  <Link href={"/signup"}>
-                    <div className="border font-bold border-slate-900 p-2 rounded-lg w-24 text-center">
-                      Sign Up
-                    </div>
-                  </Link>
-
-                  <Link href={"/login"}>
-                    <div className="bg-slate-900 font-bold p-2 rounded-lg text-white w-24 text-center">
-                      Login
-                    </div>
-                  </Link>
-                </>
-              )}
-              {user && (
-                <Link
-                  className="btn btn-sm btn-warning"
-                  href={"/signout"}>
-                  <div>Sign out</div>
-                </Link>
-              )}
-            </nav>
-          </header>
-          <main>{children}</main>
-        </div>
+      data-theme="light"
+      suppressHydrationWarning
+      >
+      <body className='bg-white dark:bg-black'>
+          <main>
+<ThemeProvider attribute="class">
+            <Navbar>
+            {children}
+            </Navbar>
+</ThemeProvider>
+          </main>
       </body>
     </html>
+
   );
 }
+
+// "use client";
+
+// import "./globals.css";
+// import Link from "next/link";
+// import { onAuthStateChanged } from "firebase/auth";
+// import { auth } from "./firebase/config";
+// import { useAuthState } from "react-firebase-hooks/auth";
+// import { useEffect, useState } from "react";
+// import { usePathname } from "next/navigation";
+// import useDrift from "./_hooks/liveChat-api/useDrift.js";
+// import Image from 'next/image';
+// import { ThemeProvider } from 'next-themes'
+// import ThemeChanger from '@/app/components/ThemeChanger';
+
+// export default function RootLayout({ children }) {
+//   const [user, loading, error] = useAuthState(auth);
+
+//   useDrift();
+//   const [shouldUseDrift, setShouldUseDrift] = useState(true);
+//   const pathname = usePathname();
+
+//   useEffect(() => {
+//     if (typeof window === "undefined") return;
+
+//     let adminURL = pathname.includes("admin");
+
+//     if (adminURL && window.drift?.api?.widget) {
+//       window.drift.api.widget.hide();
+//     } else if (!adminURL && window.drift?.api?.widget) {
+//       window.drift.api.widget.show();
+//     }
+//     setShouldUseDrift(adminURL ? false : true);
+//   }, [pathname]);
+
+//   return (
+//     <html
+//       lang="en"
+//       data-theme="light">
+//       <body>
+//         <div className="max-w-screen-2xl mx-auto px-4 py-8">
+//           <header className="flex bg-gray-100 border rounded-xl p-1 items-center">
+//           <div className="navbar marker:bg-gray-100 rounded-box">
+//             <div className="flex-1 px-2 lg:flex-none items-center justify-center">
+//               <Link href={"/"} className="text-3xl font-bold flex items-center">
+//                 <Image priority src={'/broken-pipe.svg'} alt='plumbing pipe repair' width={120} height={120}/>
+//                 Broken Pipe Plumbing
+//               </Link>
+//             </div>
+//             <div className="flex justify-end flex-1 px-2">
+//               <div className="flex items-stretch">
+//                 <Link href={"/appointment"} className="btn btn-ghost rounded-btn text-xl">Book Now</Link>
+//                 <Link href={"/companyservices"} className="btn btn-ghost rounded-btn text-xl">Services</Link>
+//                 <div className="dropdown dropdown-end">
+//                   <div tabIndex={0} role="button" className="btn btn-ghost rounded-btn text-xl">Client Portal</div>
+//                   <ul tabIndex={0} className="menu dropdown-content z-[1] p-2 shadow bg-base-100 rounded-box w-52 mt-4">
+//                   <li>
+//                       <Link href={"/signup"}>
+//                       <div className="font-bold rounded-lg">
+//                         Sign Up
+//                       </div>
+//                     </Link>
+//                     </li>
+//                     <li>
+//                       <Link href={"/login"}>
+//                       <div className="font-bold rounded-lg">
+//                         Login
+//                       </div>
+//                     </Link>
+//                     </li>
+//                   </ul>
+//                 </div>
+//                 <div className="dropdown dropdown-end">
+//                   <div tabIndex={0} role="button" className="btn btn-ghost rounded-btn text-xl">Employee Portal</div>
+//                   <ul tabIndex={0} className="menu dropdown-content z-[1] p-2 shadow bg-base-100 rounded-box w-52 mt-4">
+//                     <li>
+//                       <Link href={'/adminSignInAndSignUp'}>
+//                       <div className="font-bold rounded-lg ">
+//                         Admin Management
+//                       </div>
+//                     </Link>
+//                     </li>
+//                   </ul>
+//                 </div>
+//               </div>
+//               <ThemeChanger />
+//             </div>
+//           </div>
+//           </header>
+//           <main>
+//           <ThemeProvider attribute="class">
+//             {children}
+//           </ThemeProvider>
+//             </main>
+//         </div>
+//       </body>
+//     </html>
+
+//   );
+// }
+
