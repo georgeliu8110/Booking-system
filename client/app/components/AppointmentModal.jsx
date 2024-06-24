81592b6c1ed088a107d32520d8d0980a4b816f46
George Liu <georgeliu8110@gmail.com>
2024-06-24 00:27:35 -0500

    change customerInput state

--- client/app/components/AppointmentModal.jsx
+++ client/app/components/AppointmentModal.jsx
@@ -1,18 +1,59 @@
+"use client";
+
 import AppointmentPage from "@/app/components/AppointmentPage";
 import stepContext from "@/app/context/stepContext";
-import { useContext } from "react";
+import { useContext, useState} from "react";
 
 export default function AppointmentModal() {
 
   const stepCtx = useContext(stepContext);
+
+  const [ customerInput, setCustomerInput ] = useState({
+    service: "",
+    detail: "",
+    images: [],
+    serivceDate:"",
+    appointment: "",
+    firstName: "",
+    lastName: "",
+    email: "",
+    phone: "",
+    street: "",
+    apt: "",
+    state: "",
+    city: "",
+    zip: ""
+  })
+  const [errorInputMessage, setErrorInputMessage] = useState('');
+
+  const handleCloseModal = () => {
+    stepCtx.resetStep();
+    setCustomerInput({
+      service: "",
+      detail: "",
+      images: [],
+      serivceDate:"",
+      appointment: "",
+      firstName: "",
+      lastName: "",
+      email: "",
+      phone: "",
+      street: "",
+      apt: "",
+      state: "",
+      city: "",
+      zip: ""
+    })
+    setErrorInputMessage('')
+  }
 
   return (
     <>
     <dialog id="my_modal_3" className="modal">
       <div className="modal-box max-w-5xl h-full">
         <form method="dialog" className='relative'>
-            <AppointmentPage />
-          <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2" onClick={() => stepCtx.resetStep()}>✕</button>
+            <AppointmentPage customerInput={customerInput} setCustomerInput={setCustomerInput} errorInputMessage={errorInputMessage} setErrorInputMessage={setErrorInputMessage}/>
+          <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2" onClick={handleCloseModal}>✕</button>
         </form>
       </div>
     </dialog>
