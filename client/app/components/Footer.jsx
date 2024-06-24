101ac82d42e57a7bb8ebfb5932fadef279f212e1
George Liu <georgeliu8110@gmail.com>
2024-06-24 00:25:48 -0500

    add footer

--- client/app/components/Footer.jsx
+++ client/app/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <>
-    <footer className="footer footer-center p-10 bg-base-200 text-base-content rounded">
+    <footer className="footer footer-center p-8 bg-base-200 text-base-content rounded mt-10">
       <nav className="grid grid-flow-col gap-4">
         <a className="link link-hover">About us</a>
         <a className="link link-hover">Contact</a>
