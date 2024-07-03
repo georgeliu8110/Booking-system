9c5e478f45b6785e9866bdcfe7efac88e287a91a
George Liu <georgeliu8110@gmail.com>
2024-07-03 13:01:38 -0500

    adjust the AppointmentInfo component

--- client/app/components/AppConfirmation.jsx
+++ client/app/components/AppConfirmation.jsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 import { useSearchParams } from "next/navigation";
 import { Suspense } from "react";
 import { app } from "../firebase/config";
-import formatDateForConfirmation from '@/app/utility/formatDateForConfirmation';
+import {formatDateForConfirmation} from '@/app/utility/formatDateForConfirmation';
 
 export default function AppConfirmation() {
   return (
@@ -17,13 +17,11 @@ export default function AppConfirmation() {
 }
 
 function AppointmentInfo() {
-  const searchParams = useSearchParams();
-  console.log("searchParams", searchParams)
-  const appointment = searchParams.get("appointment");
-  console.log("appointment", appointment);
 
+  const searchParams = useSearchParams();
+  const appointment = searchParams.get("appointment");
   const parsedApp = JSON.parse(appointment);
-  console.log("parsedApp", parsedApp)
+
 
   return (
     <>
@@ -34,11 +32,17 @@ function AppointmentInfo() {
           <h1 className="text-4xl">{`Hi ${parsedApp.customerInfo.name}! Your following appointment is confirmed:`}</h1>
           <br />
           <br />
-          <h1 className="text-5xl font-bold">{`Time: ${formatDateForConfirmation(parsedApp.appointmentTime.date)} at ${timeSlots[parsedApp.appointmentTime.timeSlot]}`}</h1>
+          <h1 className="text-5xl">{`Time:`}</h1>
           <br />
-          <h1 className="text-5xl font-bold">{`Service location: ${parsedApp.customerInfo.address}`}</h1>
+          <h1 className="text-5xl font-bold underline">{`${formatDateForConfirmation(parsedApp.appointmentTime.date)} ${timeSlots[parsedApp.appointmentTime.timeSlot]}`}</h1>
+          <br />
+          <br />
+          <h1 className="text-5xl">{`Service location:`}</h1>
+          <br />
+          <h1 className="text-5xl font-bold underline">{`${parsedApp.customerInfo.address}`}</h1>
+          <br />
           <p className="py-6">You can go to client portal to manage your appointments</p>
-          <button className="btn btn-primary">View my appointment</button>
+          <Link href='/customerProfilePage'><button className="btn btn-primary">View my appointment</button></Link>
         </div>
       </div>
     </div>
