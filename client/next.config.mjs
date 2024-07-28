5c5699fe46e3f04d325026beb2a2d10a30963362
George Liu <georgeliu8110@gmail.com>
2024-07-28 14:36:43 -0500

    adjust nextjs config for pdf handling

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,4 +1,33 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+import './polyfills.mjs';
+import path from 'path';
+
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {hostname: 'firebasestorage.googleapis.com'}
+    ],
+  },
+  webpack: (config) => {
+    webpack: (config, { isServer }) => {
+      // Fixes npm packages that depend on `fs` module
+      if (!isServer) {
+        config.node = {
+          fs: 'empty'
+        }
+      }
+
+      config.module.rules.push({
+        test: /pdf\.worker\.(min\.)?js$/,
+        use: 'file-loader',
+      });
+
+
+    }
+ config.resolve.alias.canvas = false;
+   return config;
+
+}
+}
 
 export default nextConfig;
