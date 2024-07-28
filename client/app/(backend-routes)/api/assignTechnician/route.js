28541f829453602dc7108c2078d5a1ca6d939cf7
George Liu <georgeliu8110@gmail.com>
2024-07-28 14:40:06 -0500

    add the route handler for assigning technician to the appointment

---
+++ client/app/(backend-routes)/api/assignTechnician/route.js
@@ -0,0 +1,41 @@
+import { db, FieldValue } from "@/app/lib/firestore/config";
+
+export async function POST (request) {
+
+  const data = await request.json();
+  const { email, appId, technician, status } = data;
+
+  console.log('data', data)
+
+  try {
+    const customerQueryRef = await db.collection('customerInfo').where("email", "=", email);
+    const customerQuerySnapshot = await customerQueryRef.get();
+    const customerDoc = await customerQuerySnapshot.docs[0];
+    const customerDocRef = customerDoc.ref;
+    const customerDocData = customerDoc.data();
+    const updatedAppointments = customerDocData.appointments.map(appointment => {
+      if (appointment.confirmationNumber === appId) {
+        return { ...appointment, status, technician};
+      }
+      return appointment;
+    })
+
+    await customerDocRef.update({ appointments: updatedAppointments });
+
+    const body = JSON.stringify({ technician, status });
+
+    return new Response(body, {
+      status: 200,
+    });
+  } catch (error) {
+    const body = JSON.stringify({
+      data: [],
+      message: `failed to update appointment status`,
+      error: error?.message,
+    });
+
+    return new Response(body, {
+      status: 500,
+    });
+  }
+}
