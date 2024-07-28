74f65179987775c6ba14022fc36e7fa5293a0fc8
George Liu <georgeliu8110@gmail.com>
2024-07-28 14:55:12 -0500

    add customerAgreement component for customer to view the agreement PDF

---
+++ client/app/components/AdminAppCustomerAgreement.jsx
@@ -0,0 +1,82 @@
+'use client';
+
+import { useCallback, useState } from 'react';
+import { useResizeObserver } from '@wojtekmaj/react-hooks';
+import { pdfjs, Document, Page } from 'react-pdf';
+import 'core-js/features/array/at';
+import 'core-js/features/promise/all-settled';
+import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
+import 'react-pdf/dist/esm/Page/TextLayer.css';
+import 'core-js/full/promise/with-resolvers.js';
+
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+    'pdfjs-dist/legacy/build/pdf.worker.min.mjs',
+    import.meta.url,
+).toString();
+
+const options = {
+  cMapUrl: '/cmaps/',
+  standardFontDataUrl: '/standard_fonts/',
+};
+
+const resizeObserverOptions = {};
+
+const maxWidth = 1600;
+
+export default function AdminAppCustomerAgreement() {
+  const [file, setFile] = useState('/customerAgreement.pdf');
+  const [numPages, setNumPages] = useState(null);
+  const [containerRef, setContainerRef] = useState(null);
+  const [containerWidth, setContainerWidth] = useState(null);
+
+  const onResize = useCallback((entries) => {
+    const [entry] = entries;
+
+    if (entry) {
+      setContainerWidth(entry.contentRect.width);
+    }
+  }, []);
+
+  useResizeObserver(containerRef, resizeObserverOptions, onResize);
+
+  function onFileChange(event) {
+    const { files } = event.target;
+
+    const nextFile = files?.[0];
+
+    if (nextFile) {
+      setFile(nextFile);
+    }
+  }
+
+  function onDocumentLoadSuccess({ numPages: nextNumPages }) {
+    setNumPages(nextNumPages);
+  }
+
+  return (
+    <div className="flex flex-col items-center bg-gray-800 text-white min-h-screen w-full">
+      <header className="bg-gray-700 shadow-lg p-5 w-full flex justify-center">
+        <h1>Customer Agreement</h1>
+      </header>
+      <div className="flex flex-col items-center my-5 p-5 w-full max-w-screen-lg">
+        <div className="w-full max-w-full" ref={setContainerRef}>
+          <Document
+            file={file}
+            onLoadSuccess={onDocumentLoadSuccess}
+            options={options}
+            className="flex flex-col items-center"
+          >
+            {Array.from(new Array(numPages), (el, index) => (
+              <Page
+                key={`page_${index + 1}`}
+                pageNumber={index + 1}
+                width={containerWidth ? Math.min(containerWidth, maxWidth) : maxWidth}
+                className="my-4 shadow-lg"
+              />
+            ))}
+          </Document>
+        </div>
+      </div>
+    </div>
+  );
+}
