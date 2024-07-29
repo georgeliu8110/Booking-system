6c8a7b2b829a56183e1deea7ce26268b9853ad1a
George Liu <georgeliu8110@gmail.com>
2024-07-29 13:03:39 -0500

    delete unnessary files

--- client/app/utility/mockData/pdf.worker.js
+++
@@ -1,4 +0,0 @@
-import { GlobalWorkerOptions } from 'pdfjs-dist';
-
-// Use the correct worker path for the updated version
-GlobalWorkerOptions.workerSrc = require('pdfjs-dist/build/pdf.worker.entry');
