5602be2cbc950c9ee3bc1095b7f3d570af794948
George Liu <georgeliu8110@gmail.com>
2024-07-28 22:27:01 -0500

    add updateStatus route handler

---
+++ client/app/(backend-routes)/api/updateStatus/route.js
@@ -0,0 +1,38 @@
+import { db, FieldValue } from "@/app/lib/firestore/config";
+
+export async function POST(request) {
+  const appointmentData = await request.json();
+
+  const { email, appId, status } = appointmentData;
+
+  try {
+    const customerQueryRef = await db.collection('customerInfo').where("email", "=", email);
+    const customerQuerySnapshot = await customerQueryRef.get();
+    const customerDoc = await customerQuerySnapshot.docs[0];
+    const customerDocRef = customerDoc.ref;
+    const customerDocData = customerDoc.data();
+    const updatedAppointments = customerDocData.appointments.map(appointment => {
+      if (appointment.confirmationNumber === appId) {
+        return { ...appointment, status };
+      }
+      return appointment;
+    })
+
+    await customerDocRef.update({ appointments: updatedAppointments });
+
+    const body = JSON.stringify({status})
+    return new Response(body, {
+      status: 200,
+    });
+  } catch (error) {
+    const body = JSON.stringify({
+      data: [],
+      message: `failed to update appointment status`,
+      error: error?.message,
+    });
+
+    return new Response(body, {
+      status: 500,
+    });
+  }
+}
