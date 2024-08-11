fe91618279c2f710cdd1b94a0b21baf7841c689a
George Liu <georgeliu8110@gmail.com>
2024-08-11 15:49:47 -0500

    update updateStatus route handler

--- client/app/(backend-routes)/api/updateStatus/route.js
+++ client/app/(backend-routes)/api/updateStatus/route.js
@@ -2,37 +2,51 @@ import { db, FieldValue } from "@/app/lib/firestore/config";
 
 export async function POST(request) {
   const appointmentData = await request.json();
-
   const { email, appId, status } = appointmentData;
 
   try {
-    const customerQueryRef = await db.collection('customerInfo').where("email", "=", email);
+    const customerQueryRef = db.collection('customerInfo').where("email", "=", email).limit(1);
     const customerQuerySnapshot = await customerQueryRef.get();
-    const customerDoc = await customerQuerySnapshot.docs[0];
+
+    if (customerQuerySnapshot.empty) {
+      return new Response(JSON.stringify({
+        message: 'No customer found with the provided email',
+      }), { status: 404 });
+    }
+
+    const customerDoc = customerQuerySnapshot.docs[0];
     const customerDocRef = customerDoc.ref;
-    const customerDocData = customerDoc.data();
-    const updatedAppointments = customerDocData.appointments.map(appointment => {
-      if (appointment.confirmationNumber === appId) {
-        return { ...appointment, status };
+
+    await db.runTransaction(async (transaction) => {
+      const customerDocSnapshot = await transaction.get(customerDocRef);
+
+      if (!customerDocSnapshot.exists) {
+        throw new Error("Customer document does not exist.");
       }
-      return appointment;
-    })
 
-    await customerDocRef.update({ appointments: updatedAppointments });
+      const customerDocData = customerDocSnapshot.data();
+      const updatedAppointments = customerDocData.appointments.map(appointment => {
+        if (appointment.confirmationNumber === appId) {
+          return { ...appointment, status };
+        }
+        return appointment;
+      });
 
-    const body = JSON.stringify({status})
-    return new Response(body, {
-      status: 200,
+      transaction.update(customerDocRef, { appointments: updatedAppointments });
     });
+
+    const body = JSON.stringify({ status });
+    return new Response(body, { status: 200 });
+
   } catch (error) {
+    console.error("Failed to update appointment status:", error.message);
+
     const body = JSON.stringify({
       data: [],
-      message: `failed to update appointment status`,
-      error: error?.message,
+      message: 'Failed to update appointment status',
+      error: error.message,
     });
 
-    return new Response(body, {
-      status: 500,
-    });
+    return new Response(body, { status: 500 });
   }
 }
