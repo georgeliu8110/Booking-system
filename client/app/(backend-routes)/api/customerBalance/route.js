c9b3c42052951746e041526d739fb391d8a20605
George Liu <georgeliu8110@gmail.com>
2024-08-11 15:48:14 -0500

    remove console.logs

--- client/app/(backend-routes)/api/customerBalance/route.js
+++ client/app/(backend-routes)/api/customerBalance/route.js
@@ -5,13 +5,10 @@ export async function GET(request) {
   const params = request.nextUrl.searchParams;
   const email = params.get('email');
 
-  console.log('email111', email)
-
   try {
     const customerRef = db.collection('customerInfo');
     const customerQuery = await customerRef.where('email', '==', email).get();
     const customerData = customerQuery.docs.map(doc => doc.data());
-    console.log('customerData', customerData)
     const appointmentsInvoiced = customerData[0].appointments.filter(appointment => appointment.status === 'Invoiced');
     const customerBalance = appointmentsInvoiced.reduce((acc, appointment) => {
       acc += Number(appointment.invoiceBalance);
